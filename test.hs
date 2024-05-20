08b7c3417ba5fc86093a614f7eb3c9a8487b9113
Greg Muller <39238873+gnmuller@users.noreply.github.com>
2024-05-20 11:12:30 -0500

    Add files via upload

--- test.hs
+++ test.hs
@@ -1,3 +1,8 @@
+--Load in Terminal this page
+-- :l test
+-- Could also be :l test.hs
+
+
 doubleMe x = x + x
 
 
@@ -34,3 +39,61 @@ boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 
 --_ turns it into a blank it is still a list
 length' xs = sum [1 | _ <- xs] 
+
+lucky :: (Integral a) => a -> String
+lucky 7 = "LUCKY NUMBER SEVEN!"
+lucky x = "Sorry, you're out of luck, pal!" 
+
+
+charName :: Char -> String
+charName 'a' = "Albert"
+charName 'b' = "Broseph"
+charName 'c' = "Cecil"
+
+--head' "loser" 
+--Not [loser] because "loser" is already a list
+head' :: [a] -> a
+head' [] = error "Can't call head on an empty list, dummy!"
+head' (x:_) = x
+
+---length1' [1,2,2,2]
+length1' :: (Num b) => [a] -> b
+length1' [] = 0
+length1' (_:xs) = 1 + length' xs
+
+
+
+--First use of otherwise and declaring a variable where
+bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell weight height
+    | bmi <= skinny = "You're underweight, you emo, you!"
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | bmi <= fat    = "You're fat! Lose some weight, fatty!"
+    | otherwise     = "You're a whale, congratulations!"
+    where bmi = weight / height ^ 2
+          skinny = 18.5
+          normal = 25.0
+          fat = 30.0
+
+--What I don't like is that f and l aren't really strickly said
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
+    where (f:_) = firstname
+          (l:_) = lastname  
+
+
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+    | a > b     = GT
+    | a == b    = EQ
+    | otherwise = LT
+
+
+--(let a = 100; b = 200; c = 300 in a*b*c, let foo="Hey "; bar = "there!" in foo ++ bar)
+
+
+describeList :: [a] -> String
+describeList xs = "The list is " ++ what xs
+    where what [] = "empty."
+          what [x] = "a singleton list."
+          what xs = "a longer list."
